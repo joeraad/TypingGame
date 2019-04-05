2dfc15666f7c8e6bdd43f0a1f445765f8f03d267
phoenix <joeraad12@gmail.com>
2019-04-05 15:11:54 +0300

    training mode first steps
    making the keyboard layout GUI

---
+++ Assets/TrainingWordDisplay.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+
+public class TrainingWordDisplay : MonoBehaviour
+{
+
+    public TextMeshProUGUI textField;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textField.text = "";
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+
+
+}
