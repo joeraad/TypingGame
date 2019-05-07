7abc64eaedcf0c8e9459a912f4072994c7a72c39
Joe Raad <joeraad12@gmail.com>
2019-05-07 19:18:56 +0300

    Added character color changes

--- Assets/TrainingGameManager.cs
+++ Assets/TrainingGameManager.cs
@@ -12,6 +12,7 @@ public class TrainingGameManager :MonoBehaviour
     public Keyboard keyboard;
     public GameObject activeKey;
     public GameObject [] keys;
+    public TrainingWordDisplay wordDisplay;
     public Dictionary<string, GameObject> Key = new Dictionary<string, GameObject>();
     private string[] keyNames= { "`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "BackSpace",
         "Tab", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "\\",
@@ -30,7 +31,6 @@ public class TrainingGameManager :MonoBehaviour
     {
         CreateDictionary();
         text = textField.text;
-        
 
         /**
         *foreach (KeyValuePair<string, GameObject> k in Key)
@@ -51,7 +51,7 @@ public class TrainingGameManager :MonoBehaviour
         foreach (char letter in Input.inputString)
         {
             CheckLetter(letter.ToString());
-            ChangeColor(letter.ToString());
+            
         }
     }
 
@@ -63,28 +63,23 @@ public class TrainingGameManager :MonoBehaviour
             return;
         }
 
-
-        if (text[textIndex].Equals(letter))
+        Debug.Log(text[textIndex]);
+        Debug.Log(letter);
+        Debug.Log(text[textIndex].ToString().Equals(letter.ToString()));
+        if (text[textIndex].ToString().Equals(letter.ToString()))
         {
             keyboard.ChangeKeyColor(Key[letter.ToString()], true);
+            wordDisplay.ChangeCharacterColor(textIndex,true);
             textIndex++;
         }else
         {
             keyboard.ChangeKeyColor(Key[letter.ToString()], false);
+            wordDisplay.ChangeCharacterColor(textIndex,false);
             textIndex++;
         }
 
     }
-    void ChangeColor(string letter)
-    {
-        if (activeKey != null)
-        {
-            activeKey.GetComponent<Image>().color = Color.white;
-        }
-        activeKey = Key[letter.ToString()];
-        Debug.Log(activeKey);
-        activeKey.GetComponent<Image>().color=Color.green;
-    }
+   
 
     void CreateDictionary()
     {
@@ -114,59 +109,81 @@ public class TrainingGameManager :MonoBehaviour
     }
     bool CheckCommandKeys()
     {
+        if (Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return))
+        {
+            if (text[textIndex].ToString().Equals("\n"))
+            {
+                keyboard.ChangeKeyColor(Key["ENTER"], true);
+                textIndex++;
+                return true;
+            }
+            else
+            {
+                keyboard.ChangeKeyColor(Key["ENTER"], false);
+                textIndex++;
+                return true;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (text[textIndex].ToString().Equals(" "))
+            {
+                keyboard.ChangeKeyColor(Key["SPACE"], true);
+                textIndex++;
+                return true;
+            }
+            else
+            {
+                keyboard.ChangeKeyColor(Key["SPACE"], false);
+                textIndex++;
+                return true;
+            }
+            
+        } 
         if (Input.GetKeyDown(KeyCode.Backspace))
         {
-            ChangeColor("BackSpace");
+            keyboard.ChangeKeyColor(Key["BackSpace"], true);
             return true;
         }
         if (Input.GetKeyDown(KeyCode.Tab))
         {
-            ChangeColor("Tab");
+            keyboard.ChangeKeyColor(Key["Tab"], true);
             return true;
         }
+
         if (Input.GetKeyDown(KeyCode.CapsLock))
         {
-            ChangeColor("CAPS");
-            return true;
-        }
-        if (Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return))
-        {
-            ChangeColor("ENTER");
+            keyboard.ChangeKeyColor(Key["CAPS"], true);
             return true;
         }
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
-            ChangeColor("LEFTSHIFT");
+            keyboard.ChangeKeyColor(Key["LEFTSHIFT"], true);
             return true;
         }
         if (Input.GetKeyDown(KeyCode.RightShift))
         {
-            ChangeColor("RIGHTSHIFT");
+            keyboard.ChangeKeyColor(Key["RIGHTSHIFT"], true);
             return true;
         }
         if (Input.GetKeyDown(KeyCode.LeftControl))
         {
-            ChangeColor("LEFTCRTL");
+            keyboard.ChangeKeyColor(Key["LEFTCRTL"], true);
             return true;
         }
         if (Input.GetKeyDown(KeyCode.LeftAlt))
         {
-            ChangeColor("LEFTALT");
-            return true;
-        }
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            ChangeColor("SPACE");
+            keyboard.ChangeKeyColor(Key["LEFTALT"], true);
             return true;
         }
         if (Input.GetKeyDown(KeyCode.RightAlt))
         {
-            ChangeColor("RIGHTALT");
+            keyboard.ChangeKeyColor(Key["RIGHTALT"], true);
             return true;
         }
         if (Input.GetKeyDown(KeyCode.RightControl))
         {
-            ChangeColor("RIGHTCRTL");
+            keyboard.ChangeKeyColor(Key["RIGHTCRTL"], true);
             return true;
         }
         return false;
--- Assets/TrainingWordDisplay.cs
+++ Assets/TrainingWordDisplay.cs
@@ -10,10 +10,12 @@ public class TrainingWordDisplay : MonoBehaviour
 
     public TextMeshProUGUI textField;
 
+    public TMP_CharacterInfo CharacterInfo;
+    public GameObject panel;
     // Start is called before the first frame update
     void Start()
     {
-        textField.text = "This cow is big!";
+        textField.text = "This cow is\nbig!";
     }
 
 
@@ -23,8 +25,168 @@ public class TrainingWordDisplay : MonoBehaviour
         
     }
 
+    public void ChangeCharacterColor(int index, bool isCorrect)
+    {
+        TMP_TextInfo textInfo = textField.textInfo;
+        Color32[] newVertexColors;
+        Color32 c0 = textField.color;
+        int characterCount = textInfo.characterCount;
+
+        // If No Characters then just yield and wait for some text to be added
+        if (characterCount == 0)
+        {
+            return;
+        }
 
 
+
+        CharacterInfo = textInfo.characterInfo[index];
+/*
+        float CharacterTopLeftPositionX = CharacterInfo.vertex_TL.position.x;
+        float CharacterTopLeftPositionY = CharacterInfo.vertex_TL.position.y;
+        float CharacterBottomRightPositionX = CharacterInfo.vertex_BR.position.x;
+        float CharacterBottomRightPositionY = CharacterInfo.vertex_BR.position.y;
+ */
+        float CharacterTopLeftPositionX = CharacterInfo.topLeft.x;
+        float CharacterTopLeftPositionY = CharacterInfo.topLeft.y;
+        float CharacterBottomRightPositionX = CharacterInfo.bottomLeft.x;
+        float CharacterBottomRightPositionY = CharacterInfo.bottomLeft.y;
+
+        Debug.Log("These are the points:\n" + CharacterTopLeftPositionX + " " + CharacterTopLeftPositionY + "\n" + CharacterBottomRightPositionX + " " + CharacterBottomRightPositionY);
+
+        Vector3 characterPosition = transform.TransformVector(((CharacterTopLeftPositionX + CharacterBottomRightPositionX) / 2), ((CharacterTopLeftPositionY + CharacterBottomRightPositionY) / 2), 0);
+        Debug.Log(characterPosition);
+        panel.transform.position = characterPosition;
+
+        //panel.transform.position= textField.transform.localToWorldMatrix.MultiplyPoint(characterPosition);
+        //textField.transform.localToWorldMatrix.MultiplyPoint(characterPosition);
+
+
+        /*
+                CharacterInfo = textInfo.characterInfo[index];
+                Vector3 characterPositions=CharacterInfo.vertex_BR.position;
+                panel.transform.position = textField.transform.localToWorldMatrix.MultiplyPoint3x4(characterPositions);
+        */
+
+        // Get the index of the material used by the current character.
+        int materialIndex = textInfo.characterInfo[index].materialReferenceIndex;
+        // Get the vertex colors of the mesh used by this text element (character or sprite).
+        newVertexColors = textInfo.meshInfo[materialIndex].colors32;
+        // Get the index of the first vertex used by this text element.
+        int vertexIndex = textInfo.characterInfo[index].vertexIndex;
+        // Only change the vertex color if the text element is visible.
+        if (textInfo.characterInfo[index].isVisible)
+        {
+            if (isCorrect)
+            {
+                c0 = new Color32(0, 255, 0, 255);
+            }
+            else
+            {
+                c0 = new Color32(255, 0, 0, 255);
+            }
+           
+            newVertexColors[vertexIndex + 0] = c0;
+            newVertexColors[vertexIndex + 1] = c0;
+            newVertexColors[vertexIndex + 2] = c0;
+            newVertexColors[vertexIndex + 3] = c0;
+
+            // New function which pushes (all) updated vertex data to the appropriate meshes when using either the Mesh Renderer or CanvasRenderer.
+            textField.UpdateVertexData(TMP_VertexDataUpdateFlags.Colors32);
+
+            // This last process could be done to only update the vertex data that has changed as opposed to all of the vertex data but it would require extra steps and knowing what type of renderer is used.
+            // These extra steps would be a performance optimization but it is unlikely that such optimization will be necessary.
+        }
+
+        
+       
+        
+
+    }
+
+    /*
+    using UnityEngine;
+    using System.Collections;
+
+
+    namespace TMPro.Examples
+    {
+
+        public class VertexColorCycler : MonoBehaviour
+        {
+
+            private TMP_Text m_TextComponent;
+
+            void Awake()
+            {
+                m_TextComponent = GetComponent<TMP_Text>();
+            }
+
+
+            void Start()
+            {
+                StartCoroutine(AnimateVertexColors());
+            }
+
+
+            /// <summary>
+            /// Method to animate vertex colors of a TMP Text object.
+            /// </summary>
+            /// <returns></returns>
+            IEnumerator AnimateVertexColors()
+            {
+                TMP_TextInfo textInfo = m_TextComponent.textInfo;
+                int currentCharacter = 0;
+
+                Color32[] newVertexColors;
+                Color32 c0 = m_TextComponent.color;
+
+                while (true)
+                {
+                    int characterCount = textInfo.characterCount;
+
+                    // If No Characters then just yield and wait for some text to be added
+                    if (characterCount == 0)
+                    {
+                        yield return new WaitForSeconds(0.25f);
+                        continue;
+                    }
+
+                    // Get the index of the material used by the current character.
+                    int materialIndex = textInfo.characterInfo[currentCharacter].materialReferenceIndex;
+
+                    // Get the vertex colors of the mesh used by this text element (character or sprite).
+                    newVertexColors = textInfo.meshInfo[materialIndex].colors32;
+
+                    // Get the index of the first vertex used by this text element.
+                    int vertexIndex = textInfo.characterInfo[currentCharacter].vertexIndex;
+
+                    // Only change the vertex color if the text element is visible.
+                    if (textInfo.characterInfo[currentCharacter].isVisible)
+                    {
+                        c0 = new Color32((byte)Random.Range(0, 255), (byte)Random.Range(0, 255), (byte)Random.Range(0, 255), 255);
+
+                        newVertexColors[vertexIndex + 0] = c0;
+                        newVertexColors[vertexIndex + 1] = c0;
+                        newVertexColors[vertexIndex + 2] = c0;
+                        newVertexColors[vertexIndex + 3] = c0;
+
+                        // New function which pushes (all) updated vertex data to the appropriate meshes when using either the Mesh Renderer or CanvasRenderer.
+                        m_TextComponent.UpdateVertexData(TMP_VertexDataUpdateFlags.Colors32);
+
+                        // This last process could be done to only update the vertex data that has changed as opposed to all of the vertex data but it would require extra steps and knowing what type of renderer is used.
+                        // These extra steps would be a performance optimization but it is unlikely that such optimization will be necessary.
+                    }
+
+                    currentCharacter = (currentCharacter + 1) % characterCount;
+
+                    yield return new WaitForSeconds(0.05f);
+                }
+            }
+
+        }
+    }
+    */
 
 
 }
